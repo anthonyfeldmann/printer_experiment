7768766c2038e8b8a05e92dbaf62df7c423b68f0
anthonyfeldmann <anthonyjfeldmann@gmail.com>
2026-07-07 12:36:10 -0500

    Update camera_driver.py

--- src/printer_experiment/camera_driver.py
+++ src/printer_experiment/camera_driver.py
@@ -52,7 +52,7 @@ def get_single_measurement(image_path: str, target_bucket: int = 2) -> float:
 
         # 2. SAVE THRESHOLD
         # Note: You may need to adjust the '60' based on your recent lighting tuning!
-        _, thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY_INV) 
+        _, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY_INV) 
         
         thresh_path = f"{base_name}_{timestamp}_bucket_{target_bucket}_thresh{ext}"
         cv2.imwrite(thresh_path, thresh)
